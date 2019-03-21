49fdc5d301d0c173aa608c2be13c8ed922528aca
Carlos Esquivel <carlosrem70@gmail.com>
2019-03-21 02:07:26 -0600

    Full color & format output
    it's beautiful

--- Working/Client.c
+++ Working/Client.c
@@ -5,7 +5,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
 #include <signal.h>
 #include <arpa/inet.h>
 
@@ -26,88 +26,85 @@ void error(const char *msg)
 int main(int argc, char *argv[])
 {
     int portno;
-	int myportno;
+    int myportno;
     char ipAddress[20];
-	
-	struct sockaddr_in myAddress;
+
+    struct sockaddr_in myAddress;
     char finalUser[50];
     struct hostent *server;
     int random;
-    bzero(ipAddress,20);
-    
+    bzero(ipAddress, 20);
 
     if (argc < 2) {
        fprintf(stderr,"usage: %s username\n", argv[0]);
        exit(0);
     }
-    
+
     if(argc == 2){
         strcpy(finalUser, "1");
         strcat(finalUser, argv[1]);
     }else
         strcpy(finalUser, "Nel");
-    
+
     portno = getPortNumber();
-    
+    //server = gethostbyname(argv[1]);
     getIPAddress(ipAddress);
-    
-    printf("Port:%d\n",portno);
+
+    printf("\033[1;95m#= Port: %d\n",portno);
     server = gethostbyname(ipAddress);
-	
+
     if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
+        fprintf(stderr,"\033[91m!!> ERROR, no such host <!!\033[0m \n");
         exit(0);
     }
-	
+
     int socialSocket = socket(AF_INET, SOCK_DGRAM, 0);
     myportno = getPortNumberClient();
-    printf("Client port:%d\n",myportno);
+    printf("#= Client Port: %d\n",myportno);
 
-	bzero((char *) &myAddress, sizeof(myAddress));
-	myAddress.sin_family = AF_INET;
+    bzero((char *) &myAddress, sizeof(myAddress));
+    myAddress.sin_family = AF_INET;
     myAddress.sin_addr.s_addr = INADDR_ANY;
-    myAddress.sin_port = htons(myportno);
+    myAddress.sin_port = htons(myportno); //aqui es donde se pone el puerto del cliente
 
-	if (bind(socialSocket, (struct sockaddr *) &myAddress, sizeof(myAddress)) < 0){
-    	error("ERROR on binding socket to address");
-		return 0;
-	}
-	
+    if (bind(socialSocket, (struct sockaddr *) &myAddress, sizeof(myAddress)) < 0){
+        error("\033[91m!!> ERROR on binding socket to address <!! \033[0m\n");
+        return 0;
+    }
 
-	struct sockaddr_in     servUDP, servUDPGeneral; 
+	struct sockaddr_in     servUDP, servUDPGeneral;
 
 	bzero((char *) &servUDP, sizeof(servUDP));
-	servUDP.sin_family = AF_INET; 
-    servUDP.sin_port = htons(portno); 
-    bcopy((char *)server->h_addr, 
+	servUDP.sin_family = AF_INET;
+    servUDP.sin_port = htons(portno);
+    bcopy((char *)server->h_addr,
          (char *)&servUDP.sin_addr.s_addr,
          server->h_length);
 
 	bzero((char *) &servUDPGeneral, sizeof(servUDPGeneral));
-	servUDPGeneral.sin_family = AF_INET; 
-    servUDPGeneral.sin_port = htons(portno + 1); 
-    bcopy((char *)server->h_addr, 
+	servUDPGeneral.sin_family = AF_INET;
+    servUDPGeneral.sin_port = htons(portno + 1);
+    bcopy((char *)server->h_addr,
          (char *)&servUDPGeneral.sin_addr.s_addr,
          server->h_length);
-	
-	int n;
-    socklen_t len; 
-    char existingClient[2];
-    bzero(existingClient,2);
-    len  = sizeof(servUDP);
-    sendto(socialSocket, (const char *)finalUser, strlen(finalUser), 
-        	MSG_CONFIRM, (const struct sockaddr *) &servUDP,  
-            sizeof(servUDP)); 
-	n = recvfrom(socialSocket, (char *)existingClient, 2, MSG_WAITALL, (struct sockaddr *) &servUDP, &len); 
-    if(existingClient[0] == '1'){
-        printf("User already registred.\n");
-        return 0;
-    }else{
-        printf("Connected!.\n"); 
-        standByYou(&servUDPGeneral, socialSocket);
 
-        return 0;
+	int n;
+    socklen_t len;
+    char existingClient[2];
+    len = sizeof(servUDP);
+    sendto(socialSocket, (const char *)finalUser, strlen(finalUser),
+        	MSG_CONFIRM, (const struct sockaddr *) &servUDP,
+            sizeof(servUDP));
+
+    n = recvfrom(socialSocket, (char *)existingClient, 2, MSG_WAITALL, (struct sockaddr *) &servUDP, &len);
+    if(existingClient[0] == '1') {
+        printf("\033[93m}> User already registered!\033[0m \n");
     }
+    else {
+        printf("}> Connected!\033[0m \n=-=-=-=\n");
+  	    standByYou(&servUDPGeneral, socialSocket);
+    }
+    return 0;
 }
 
 void standByYou(struct sockaddr_in* serv_addr, int sockUDP){
@@ -118,16 +115,17 @@ void standByYou(struct sockaddr_in* serv_addr, int sockUDP){
 	pid = fork();
 
 	if(pid < 0){
-		error("ERROR on fork");
+		error("\033[91m!!> ERROR on fork <!! \033[0m \n");
 		return;
 	}
+
 	while(!breakUpFlag){
 		//forked process that will read
 		if (pid == 0){
 			bzero(buffer,256);
-			n = recvfrom(sockUDP, (char *)buffer, 256, MSG_WAITALL, (struct sockaddr *) serv_addr, &len); 
-			buffer[n] = '\0'; 
-			printf(">%s", buffer); 
+			n = recvfrom(sockUDP, (char *)buffer, 256, MSG_WAITALL, (struct sockaddr *) serv_addr, &len);
+			buffer[n] = '\0';
+			printf("%s", buffer);
 		}
 		//original process that will write
 		else{
@@ -137,25 +135,27 @@ void standByYou(struct sockaddr_in* serv_addr, int sockUDP){
             if(rightMessageFormat){
                 if(isCommand(buffer)){
                     if (buffer[5] == 'e'){
-                        
-                        breakUpFlag = 1;	
+                        //kill(thirdSonId,SIGKILL);
+                        breakUpFlag = 1;
                     }
                 }
                 int n = sendto(sockUDP,(const char *) buffer, 255, MSG_CONFIRM, (const struct sockaddr *) serv_addr, sizeof(*serv_addr));
-                if (n < 0) 
-                    error("ERROR writing to socket");
-            }else{
-                printf("ERROR, wrong message format.\n");
-                printf("Usage => username: message\n");   
+                if (n < 0)
+                    error("\033[91m!!> ERROR writing to socket <!!\033[0m \n");
+            }
+            else {
+            	printf("\n\033[91m!!> ERROR, wrong message format!\033[0m\n");
+            	printf("\033[93m:: Usage => username: message \033[0m\n");
             }
 		}
 	}
+    close(sockUDP);
 }
 
 //checks for command input. Format: "comm _command symbol_"
 short isCommand(char* input){
 	int s = strlen(input);
-	
+
 	if (s >= 7 &&*(input) == 'c' && *(++input) == 'o' && *(++input) == 'm' && *(++input) == 'm' && *(++input) == ' '){
 		return 1;
 	}
@@ -167,31 +167,31 @@ int getPortNumber(){
 	int i = 0;
 	int e = 0;
 	FILE *configFile = fopen("portNumber.ini","r");
-	
+
 	if(configFile == NULL){
-		printf("ERROR opening file\n");
+		printf("\n\033[91m!!> ERROR opening file <!!\033[0m \n");
 		exit(-1);
 	}
-	
+
 	char ct[100];
 	bzero((char *)&ct[0], sizeof(ct));
 	fscanf(configFile,"%s",(char *)&ct[0]);
 	char *c = &ct[0];
 	fclose(configFile);
-	
+
 	while(*(c+i) == 'p' || *(c+i) == 'o' || *(c+i) == 'r' || *(c+i) == 't' || *(c+i) == 'N' ||
 		  *(c+i) == 'u' || *(c+i) == 'm' || *(c+i) == 'b' || *(c+i) == 'e' || *(c+i) == 'r' ||
 		  *(c+i) == '=')
 		i++;
-		
+
 	char portN[7];
-	
+
 	while(c[i] != ';'){
 		portN[e] = c[i];
 		i++;
 		e++;
 	}
-	
+
 	port = atoi(&portN[0]);
 	return port;
 }
@@ -217,33 +217,33 @@ int getPortNumberClient(){
 	int i = 0;
 	int e = 0;
 	FILE *configFile = fopen("portNumber.ini","r");
-	
+
 	if(configFile == NULL){
-		printf("ERROR opening file\n");
+		printf("\n\033[91m!!> ERROR opening file <!!\033[0m \n");
 		exit(-1);
 	}
-	
+
 	char ct[200];
 	bzero((char *)&ct[0], sizeof(ct));
 	fscanf(configFile,"%s",(char *)&ct[0]);
 	char *c = strchr(ct,'$');
 	fclose(configFile);
 	c++;
-	
+
 	while(*c == 'p' || *c == 'o' || *c == 'r' || *c == 't' || *c == 'N' || *c == 'u' || *c == 'm' ||
 		  *c == 'b' || *c == 'e' || *c == 'C' || *c == 'l' || *c == 'i' || *c == 'n' || *c == 't' ||
 		  *c == '=')
 		c++;
-		
+
 	char portN[7];
 	bzero(portN,7);
-	
+
 	while(*c != ';'){
 		portN[e] = *c;
 		e++;
 		c++;
 	}
-	
+
 	port = atoi(&portN[0]);
 
 	return port;
@@ -253,23 +253,23 @@ void getIPAddress(char *ip){
 	bzero(ip,sizeof(ip));
 	int e = 0;
 	FILE *configFile = fopen("portNumber.ini","r");
-	
+
 	if(configFile == NULL){
-		printf("ERROR opening file\n");
+		printf("\n\033[91m!!> ERROR opening file<!!\033[0m \n");
 		exit(-1);
 	}
-	
+
 	char ct[200];
 	bzero((char *)&ct[0], sizeof(ct));
 	fscanf(configFile,"%s",(char *)&ct[0]);
 	char *c = strchr(ct,'?');
 	fclose(configFile);
 	c++;
-	
+
 	while(*c == 'i' || *c == 'p' || *c == 'A' || *c == 'd' || *c == 'r' || *c == 'e' || *c == 's' ||
 		  *c == '=')
 		c++;
-	
+
 	while(*c != ';'){
 		ip[e] = *c;
 		e++;
--- Working/Server.c
+++ Working/Server.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <signal.h>
@@ -15,18 +15,20 @@ char digits[10] = {'0','1','2','3','4','5','6','7','8','9'};
 
 int getRandom(int,int);
 char* generateId(char*);
+unsigned short generateColor();
 int getPortNumber();
 short verifyUserAssignation(char *user);
 char *getUser(char *, char *);
 void getUserIdFromMessage(char *, char *);
+unsigned short getColorId(char *);
 void getMessage(char *, char *);
-void getIPAddress(char *);
-int findExistingClient(char *);
-void addOnlineUsersToMessage(char *);
+void getIPAddress(char*);
+int findExistingClient(char*);
+void addOnlineUsersToMessage(char*);
 
-struct infoCard{
+struct infoCard {
 	char userId[40];
-	
+	unsigned short colorId;
 };
 
 struct user{
@@ -42,6 +44,7 @@ void addOnlineUser(struct infoCard* info, struct sockaddr_in* address){
 	for (i = 0; i < 100; i++){
 		if((*(users+i)).online == 0){
 			strcpy((*(users+i)).info.userId, (*info).userId);
+			(*(users+i)).info.colorId = (*info).colorId;
 			(*(users+i)).addr = *address;
 			(*(users+i)).online = 1;
 			return;
@@ -51,11 +54,9 @@ void addOnlineUser(struct infoCard* info, struct sockaddr_in* address){
 
 struct user* findOnlineUser(char* userId){
 	int i;
-	for (i = 0; i < 100; i++){
-		if((*(users+i)).online == 1 && strcmp((*(users+i)).info.userId, userId) == 0){
-			
+	for (i = 0; i < 100; i++) {
+		if((*(users+i)).online == 1 && strcmp((*(users+i)).info.userId, userId) == 0)
 			return users+i;
-		}
 	}
 	return NULL;
 }
@@ -68,42 +69,41 @@ char *findUserByAddress(struct sockaddr_in* address){
 
 	bzero(bufferUser, lenUser);
 	inet_ntop(AF_INET, &((*address).sin_addr), bufferUser, lenUser);
- 
+
 	for (i = 0; i < 100; i++){
-		
+
 		if((*(users+i)).online == 1){
 
 			bzero(bufferTemp, lenTemp);
-			inet_ntop(AF_INET, &((*(users+i)).addr.sin_addr), bufferTemp, lenTemp);	
+			inet_ntop(AF_INET, &((*(users+i)).addr.sin_addr), bufferTemp, lenTemp);
 
-			if(strcmp(bufferUser, bufferTemp) == 0 && ((*(users+i)).addr.sin_port == (*address).sin_port)){
+			if(strcmp(bufferUser, bufferTemp) == 0 && ((*(users+i)).addr.sin_port == (*address).sin_port)) {
 				return (*(users+i)).info.userId;
-			}	
-		}		
-	
+			}
+		}
 	}
 }
 
-void removeOnlineUser(char* userId){
+void removeOnlineUser(char* userId) {
 	struct user* downUser = findOnlineUser(userId);
-	
+
 	(*(downUser)).online = 0;
 }
 
-void printOnlineUsers(){
+void printOnlineUsers() {
 	int i;
-	int len=20;
+	int len = 20;
 	char buffer[len];
-	
 
-	for (i = 0; i < 100; i++){
-		if((*(users+i)).online == 1){
-			printf("user: %s \n",(*(users+i)).info.userId);
+
+	for (i = 0; i < 100; i++) {
+		if((*(users+i)).online == 1) {
+			printf("Username:\033[%dm %s \033[0m \n", ((*(users+i)).info.colorId), ((*(users+i)).info.userId));
 			inet_ntop(AF_INET, &((*(users+i)).addr.sin_addr), buffer, len);
-			printf("address: %s\n", buffer);
+			printf("Address: %s\n", buffer);
 			int puerto = ntohs((*(users+i)).addr.sin_port);
-			printf("port: %d\n", puerto);
-			printf("-------------------------------------\n");
+			printf("Port: %d\n", puerto);
+			printf("\033[97m-------------------------------------\033[0m \n");
 		}
 	}
 }
@@ -122,91 +122,91 @@ int main(int argc, char *argv[])
     srand( (unsigned) time(NULL) * getpid());//reseeds the randomgenerator
     short assignUser;
 	char *userSend;
-	
+
     //reserves shared memory for users array and initializes the array
     users = mmap(NULL, sizeof(struct user)*100, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
 	int i;
 	for (i = 0; i < 100; i++){
-		(*(users+i)).online = 0;	
+		(*(users+i)).online = 0;
 		bzero((char *) &((*(users+i)).addr), sizeof((*(users+i)).addr));
 	}
-	
-	int socketUDP;
-	int socketGeneral;
-    int portNumber; //Port number on which the server accepts connections
-    int pid;
-	int random;
-	int existingClient = 0;
-	char ipAddress[20];
-	bzero(ipAddress, 20);
+
+	int socketUDP,
+	 	socketGeneral,
+     	portNumber, //Port number on which the server accepts connections
+     	pid,
+	 	random;
+	char ipAddress[15];
+	bzero(ipAddress, 15);
     socklen_t clientAddressLength; //size of the address of the client
-	struct sockaddr_in servUDP_addr, servUDPGeneral_addr, cli_addr;//Direccion del servidor y del cliente
-   
+	struct sockaddr_in servUDP_addr, servUDPGeneral_addr, cli_addr; //Direccion del servidor y del cliente
+
     int n;	//return value for the read() and write() calls
-	
+
 	if (argc > 1){
-    	fprintf(stderr,"ERROR, port provided\n");
+    	fprintf(stderr,"\n\033[91m!!> ERROR, port provided <!!\033[0m \n");
         exit(1);
     }
-	
+
 	socketUDP = socket(AF_INET, SOCK_DGRAM, 0); //Peticiones de conectarse
-    if (socketUDP < 0) 
-       error("ERROR opening UDP server socket");
+    if (socketUDP < 0)
+       error("\n\033[91m!!> ERROR opening UDP server socket <!! \033[0m \n");
+
 	socketGeneral = socket(AF_INET, SOCK_DGRAM, 0); //Escuchar cada cliente
-    if (socketGeneral < 0) 
-       error("ERROR opening general UDP server socket");
+    if (socketGeneral < 0)
+       error("\n\033[91m!!> ERROR opening general UDP server socket <!! \033[0m \n");
 
 	bzero((char *) &servUDP_addr, sizeof(servUDP_addr));
 	bzero((char *) &servUDPGeneral_addr, sizeof(servUDPGeneral_addr));
     portNumber = getPortNumber(); //gets the port number from the config file
-	printf("Port:%d\n",portNumber);
-	
+
 	getIPAddress(ipAddress);
-	
+
 	//Setting parametes of the struct sockaddr_in
 	servUDP_addr.sin_family = AF_INET;
-    servUDP_addr.sin_addr.s_addr = inet_addr(ipAddress);
-    servUDP_addr.sin_port = htons(portNumber); 
+	servUDP_addr.sin_addr.s_addr = inet_addr(ipAddress);
+    servUDP_addr.sin_port = htons(portNumber);
 
 	servUDPGeneral_addr.sin_family = AF_INET;
-    servUDPGeneral_addr.sin_addr.s_addr = inet_addr(ipAddress);
+	servUDPGeneral_addr.sin_addr.s_addr = inet_addr(ipAddress);
     servUDPGeneral_addr.sin_port = htons(portNumber + 1);
 
 	//bind() = binds a socket to an address, assings a name to an unnamed socket. Return 1 on success
 	//The address of the current host and port number on which the server will run
 	//3 arguments: socket file descriptor, address to which is bound, size of the address to which is bound
-	if (bind(socketUDP, (struct sockaddr *) &servUDP_addr, sizeof(servUDP_addr)) < 0) 
-    	error("ERROR on binding UDP server");
-	 
-	if (bind(socketGeneral, (struct sockaddr *) &servUDPGeneral_addr, sizeof(servUDPGeneral_addr)) < 0) 
-        error("ERROR on binding general UDP server");
-	
+	if (bind(socketUDP, (struct sockaddr *) &servUDP_addr, sizeof(servUDP_addr)) < 0)
+    	error("\n\033[91m!!> ERROR on binding UDP server <!! \033[0m \n");
+
+	if (bind(socketGeneral, (struct sockaddr *) &servUDPGeneral_addr, sizeof(servUDPGeneral_addr)) < 0)
+        error("\n\033[91m!!> ERROR on binding general UDP server <!! \033[0m \n");
+
     clientAddressLength = sizeof(cli_addr);
-	
+
 	//To get the host name
 	char hostname[1024];
 	hostname[1023] = '\0';
 	gethostname(hostname, 1023);
-	printf("Hostname: %s\n", hostname);
+	printf("\033[1;95m#== Hostname: %s \n", hostname);
+
 	int lenLocalAddr=20;
 	char bufferLocalAddr[lenLocalAddr];
 	inet_ntop(AF_INET, &(servUDP_addr.sin_addr), bufferLocalAddr, lenLocalAddr);
-	printf("address: %s\n", bufferLocalAddr);
-
+	printf("#== Address: %s \n", bufferLocalAddr);
+	printf("#== Port: %d \n#=-=-=-=-=-=-=-=-=-=-=# \033[0m \n", portNumber);
 
 	while(1)
 	{
-		char tempBuf[50]; 
+		char tempBuf[50];
 		bzero((char *) tempBuf,sizeof(tempBuf)); //tempBuf needs to be clean because of possible past users
-		socklen_t lenUDP; 
+		socklen_t lenUDP;
 		lenUDP = sizeof(cli_addr);
-		recvfrom(socketUDP, (char *)tempBuf, 50,  
-		            MSG_WAITALL, ( struct sockaddr *) &cli_addr, 
-		            &lenUDP); 
+		recvfrom(socketUDP, (char *)tempBuf, 50,
+		         MSG_WAITALL, ( struct sockaddr *) &cli_addr,
+		         &lenUDP);
 		assignUser = verifyUserAssignation(tempBuf);
 		struct infoCard myInfo;
-		char *myId = malloc( (size_t)50 );
-		if (assignUser){
+		char *myId = malloc( (size_t) 50 );
+		if (assignUser) {
 			generateId(myId);
 			strcpy(myInfo.userId,myId);
 		}else{
@@ -216,16 +216,19 @@ int main(int argc, char *argv[])
 			strcpy(myInfo.userId,myId);
 			existingClient = findExistingClient(myId);//Verifica si ya existe el usuario
 		}
-		//Verify existing client
-		if(existingClient){
-			sendto(socketUDP, "1", 2, MSG_CONFIRM, (const struct sockaddr *) &cli_addr, 
-						  sizeof(cli_addr));
+		strcpy(myInfo.userId, myId);
+		if (findExistingClient(myId)) { //verifica si ya existe un usuario
+			sendto(socketUDP, "1", 2, MSG_CONFIRM, (const struct sockaddr *) &cli_addr,
+				   sizeof(cli_addr));
 			continue;
-		}else{
-			sendto(socketUDP, "0", 2, MSG_CONFIRM, (const struct sockaddr *) &cli_addr, 
-						  sizeof(cli_addr));
 		}
-		printf("---------------New client connected---------------\n"); 
+		else
+			sendto(socketUDP, "0", 2, MSG_CONFIRM, (const struct sockaddr *) &cli_addr,
+				   sizeof(cli_addr));
+
+		myInfo.colorId = generateColor();
+
+		printf("\n\033[97m---------=[ New client connected ]=---------\n");
 		int lene=20;
 		int portNewClient = 0;
 		char buffere[lene];
@@ -236,6 +239,7 @@ int main(int argc, char *argv[])
 		printf("port: %d\n--------------------------------------------\n", portNewClient);
 
 		addOnlineUser(&myInfo, &cli_addr);
+		printf("Username:\033[%dm %s \033[97m \n-------------------------------------------- \033[0m\n", myInfo.colorId, myInfo.userId);
 
 		pid = fork();
 		if(pid < 0)
@@ -254,74 +258,83 @@ void standByMe(int sockUDP){
 	n = sizeof(cli_addr);
 	char buffer[256];
 	int success;
-	
+
 	char arrayUsers[256];
 	char finalMessageUsers[256];
-	
+
 	short breakUpFlag = 0;
 	char *userMessage;
-	
+
 	while (!breakUpFlag){
 		bzero(buffer,256);
-		recvfrom(sockUDP, (char *)buffer, 256,  
-		            MSG_WAITALL, ( struct sockaddr *) &cli_addr, 
-		            &n); 
-		
-		userMessage = findUserByAddress(&cli_addr);		
+		recvfrom(sockUDP, (char *)buffer, 256,
+		         MSG_WAITALL, ( struct sockaddr *) &cli_addr,
+		         &n);
+
+		userMessage = findUserByAddress(&cli_addr); //user that sends message
 		if (n < 0)
-			error("ERROR reading from socket");
-		
-		if (isCommand(buffer)){
+			error("\n\033[91m!!> ERROR reading from socket <!! \n\033[0m");
+
+		if (isCommand(buffer)) {
 			char command = buffer[5];
-			
-			switch (command){
+
+			switch (command) {
 				case 'e':
 					breakUpFlag = 1;
+					printf("\033[%dm:->> %s has disconnected \033[0m\n", myInfo->colorId, myInfo->userId);
 					removeOnlineUser(userMessage);
-					printf("Client disconnected: %s\n",userMessage);
+					success = sendto(sockUDP, "$", 1,
+					   				 MSG_CONFIRM, (const struct sockaddr *) &cli_addr,
+						  	 		 sizeof(cli_addr));
 					break;
+
 				case 'p':
-					
-					bzero(finalMessageUsers,256);
-					printf("\nPrinting online users... \n");
+					bzero(finalMessageUsers, 256);
+					printf("\n\033[97m------=[ Printing online users.. ]=------ \033[0m \n");
 					printOnlineUsers();
-					
-					strcat(finalMessageUsers,"Printing list of users...\n");
-					addOnlineUsersToMessage((char *)arrayUsers);
-					strcat(finalMessageUsers,arrayUsers);
-					success = sendto(sockUDP, finalMessageUsers, 256,  
-					   MSG_CONFIRM, (const struct sockaddr *) &cli_addr, 
-						  sizeof(cli_addr));
-						  
+					strcat(finalMessageUsers, "Printing user list...");
+					addOnlineUsersToMessage((char*) arrayUsers);
+					strcat(finalMessageUsers, arrayUsers);
+
+					success = sendto(sockUDP, finalMessageUsers, 256,
+					   				 MSG_CONFIRM, (const struct sockaddr *) &cli_addr,
+						  	 		 sizeof(cli_addr));
+
 					break;
+
 				default:
-					printf("Invalid command!: %c \n", command);
-					
-					success = sendto(sockUDP, "Invalid command!\n", 22,  
-					   MSG_CONFIRM, (const struct sockaddr *) &cli_addr, 
-						  sizeof(cli_addr));
+					printf("\n\033[93m> Invalid command!: %c \033[0m\n", command);
+
+					success = sendto(sockUDP, "\033[93m> Invalid command!: %c \033[0m\n", 30,
+					   				 MSG_CONFIRM, (const struct sockaddr *) &cli_addr,
+						  	 		 sizeof(cli_addr));
 			}
 		}
-		else{
+		else { //Receive message from other users
 			char userId[20];
 			char message[256];
 			char finalMessage[256];
-			bzero(userId,20);
-			bzero(message,256);
-			bzero(finalMessage,256);
-			getUserIdFromMessage(buffer,userId);
-			getMessage(buffer,message);
-			strcpy(finalMessage,userMessage);
-			strcat(finalMessage,":");
-			strcat(finalMessage,message);
+			unsigned short colorMessage = getColorId(userMessage);
+			bzero(userId, 20);
+			bzero(message, 256);
+			bzero(finalMessage, 256);
+			getUserIdFromMessage(buffer, userId);
+			getMessage(buffer, message);
+			sprintf(finalMessage, "\033[4;%dm> %s:\033[0m ", colorMessage, userMessage);
+			//strcpy(finalMessage, userMessage);
+			//strcat(finalMessage, ": \033[0m");
+			strcat(finalMessage, message);
 			struct user *usuario = findOnlineUser(userId);
-			success = sendto(sockUDP, (const char *)finalMessage,256,
+			success = sendto(sockUDP, (const char *) finalMessage,256,
 				  MSG_CONFIRM, (const struct sockaddr *) &((*usuario).addr),
 				  sizeof((*usuario).addr));
 			if (success == -1)
-				printf("error sending message\n");
-			else
-				printf("message sent\n");
+				printf("Error sending message\n");//printf("0> \033[%dm%s \033[97m-> \033[%dm\"%s\" \033[97m}>\033[91m Error sending message \033[0m\n", myInfo->colorId, myInfo->userId, colorMessage, userMessage);
+			else {
+				printf("Message sent!\n");
+				/*printf("test > \033[%dm %s < \033[0m \n", colorMessage, userMessage);
+				printf("0> \033[%dm%s \033[97m-> \033[%dm%s \033[97m}> Message sent! \033[0m\n", myInfo->colorId, myInfo->userId, colorMessage, userMessage);*/
+			}
 		}
 	}
 }
@@ -329,7 +342,7 @@ void standByMe(int sockUDP){
 //checks for command input. Format: "comm _command symbol_"
 short isCommand(char* input){
 	int s = strlen(input);
-	
+
 	if (s >= 7 &&*(input) == 'c' && *(++input) == 'o' && *(++input) == 'm' && *(++input) == 'm' && *(++input) == ' '){
 		return 1;
 	}
@@ -348,14 +361,21 @@ char* generateId(char* dest){
 	return dest;
 }
 
+unsigned short generateColor() {
+	int flag = getRandom(0, 1),
+		colorId = getRandom(31, 36);
+	unsigned short dest = colorId + 60*flag;
+	return dest;
+}
+
 void getUserIdFromMessage(char *input, char *userId){
 	int i = 0;
 	char *c = input;
 	char *format = strchr(input,':');
-	
+
 	if(format == NULL){
-		printf("ERROR, wrong message format.\n");
-		error("Usage => username: message\n");
+		printf("\n\033[91m!!> ERROR, wrong message format!\033[0m\n");
+		error("\033[93m:: Usage => username: message \033[0m\n");
 		return;
 	}
 	else{
@@ -368,12 +388,23 @@ void getUserIdFromMessage(char *input, char *userId){
 	}
 }
 
+unsigned short getColorId(char* userId) {
+	int i;
+	unsigned short colorId = 39;
+	for (i = 0; i < 100; i++) {
+		if((*(users+i)).online == 1 && strcmp((*(users+i)).info.userId, userId) == 0) {
+			colorId = (*(users+i)).info.colorId;
+		}
+	}
+	return colorId;
+}
+
 void getMessage(char *input, char *message){
 	int i = 0;
 	char *format = strchr(input,':');
 	if(format == NULL){
-		printf("ERROR, wrong message format.\n");
-		error("Usage => username: message\n");
+		printf("\n\033[91m!!> ERROR, wrong message format!\033[0m\n");
+		error("\033[93m:: Usage => username: message \033[0m\n");
 		return;
 	}
 	else{
@@ -392,31 +423,31 @@ int getPortNumber(){
 	int i = 0;
 	int e = 0;
 	FILE *configFile = fopen("portNumber.ini","r");
-	
+
 	if(configFile == NULL){
 		printf("ERROR opening file\n");
 		exit(-1);
 	}
-	
+
 	char ct[100];
 	bzero((char *)&ct[0], sizeof(ct));
 	fscanf(configFile,"%s",(char *)&ct[0]);
 	char *c = &ct[0];
 	fclose(configFile);
-	
+
 	while(*(c+i) == 'p' || *(c+i) == 'o' || *(c+i) == 'r' || *(c+i) == 't' || *(c+i) == 'N' ||
 		  *(c+i) == 'u' || *(c+i) == 'm' || *(c+i) == 'b' || *(c+i) == 'e' || *(c+i) == 'r' ||
 		  *(c+i) == '=')
 		i++;
-		
+
 	char portN[7];
-	
+
 	while(c[i] != ';'){
 		portN[e] = c[i];
 		i++;
 		e++;
 	}
-	
+
 	port = atoi(&portN[0]);
 	return port;
 }
@@ -425,29 +456,30 @@ void getIPAddress(char *ip){
 	bzero(ip,sizeof(ip));
 	int e = 0;
 	FILE *configFile = fopen("portNumber.ini","r");
-	
+
 	if(configFile == NULL){
-		printf("ERROR opening file\n");
+		printf("\n\033[91m!!> ERROR opening file <!!\033[0m\n");
 		exit(-1);
 	}
-	
+
 	char ct[200];
 	bzero((char *)&ct[0], sizeof(ct));
 	fscanf(configFile,"%s",(char *)&ct[0]);
 	char *c = strchr(ct,'?');
 	fclose(configFile);
 	c++;
-	
+
 	while(*c == 'i' || *c == 'p' || *c == 'A' || *c == 'd' || *c == 'r' || *c == 'e' || *c == 's' ||
 		  *c == '=')
 		c++;
-	
+
 	while(*c != ';'){
 		ip[e] = *c;
 		e++;
 		c++;
 	}
 }
+
 
 short verifyUserAssignation(char *user){
 	short i = 1;
@@ -469,21 +501,21 @@ char* getUser(char *user, char *myUser){
 	return myUser;
 }
 
-int findExistingClient(char *userId){
+int findExistingClient(char *userId) {
 	if(findOnlineUser(userId) == NULL)
 		return 0;
 	return 1;
 }
 
-void addOnlineUsersToMessage(char *array){
+void addOnlineUsersToMessage(char *array) {
 	int i;
 	bzero(array, 256);
-	
+
 	for (i = 0; i < 100; i++){
 		if((*(users+i)).online == 1){
 			strcat(array,"User: ");
 			strcat(array,(*(users+i)).info.userId);
 			strcat(array,"\n");
 		}
-	}	
+	}
 }
